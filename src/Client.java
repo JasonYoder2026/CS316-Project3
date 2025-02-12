5d8019a903cafee0765e98661c43abdf0a05e92f
Jason Yoder <jason.yoder2@bsu.edu>
2025-02-12 14:11:41 -0500

    Finishes client side concept

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,9 @@
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Client {
     public static void main(String[] args) throws Exception {
@@ -21,39 +24,54 @@ public class Client {
     private static void menu(SocketChannel socketChannel) {
         System.out.println("Connected to server...");
         System.out.println("Commands:");
-        System.out.println("wget:<filename> - Download file from server");
+        System.out.println("get \"<filename>\" - Download file from server");
         System.out.println("ls - List files in server");
-        System.out.println("delete:<filename> - Delete file from server");
-        System.out.println("mv:<filename>:<newfilename> - Rename file in server");
-        System.out.println("upload:<filename> - Upload file to server");
+        System.out.println("delete \"<filename>\" - Delete file from server");
+        System.out.println("mv \"<filename>\" \"<newfilename>\" - Rename file in server");
+        System.out.println("ftp \"<filename>\" - Upload file to server");
         System.out.println("exit - Exit server");
         while(true) {
             System.out.println(">>");
             Scanner input = new Scanner(System.in);
-            String command = input.nextLine();
-            //bytes =
-            processCommand(command);
-            //socketChannel.write(ByteBuffer.wrap(command.getBytes()));
+            String command = input.nextLine().trim();
+            String[] commandParts = getCommandParts(command);
+            ByteBuffer commandBytes = processCommand(commandParts);
+            try {
+                socketChannel.write(commandBytes);
+            } catch (Exception e) {
+                System.err.print("Error sending command to server.\n");
+            }
         }
     }
 
-    private static void processCommand(String command) {
-        String[] commandParts = command.split(":");
+    private static String[] getCommandParts(String command) {
+        Pattern pattern = Pattern.compile("\"([^\"]+)\"|(\\S+)");
+        Matcher matcher = pattern.matcher(command);
+        String[] commandParts = new String[3];
+        int i = 0;
+        while (matcher.find() && i < commandParts.length) {
+            commandParts[i++] = matcher.group(1) != null ? matcher.group(1) : matcher.group(2);
+        }
+        return commandParts;
+    }
+
+    private static ByteBuffer processCommand(String[] commandParts) {
+        ByteBuffer commandBytes =null;
         switch (commandParts[0].toLowerCase()) {
-            case ("wget"):
-                ClientCommands.downloadFile(commandParts[1]);
+            case ("get"):
+                commandBytes = ByteBuffer.wrap(("d" + commandParts[1]).getBytes());
                 break;
             case ("ls"):
-                listFiles();
+                commandBytes = ByteBuffer.wrap("l".getBytes());
                 break;
             case ("delete"):
-                deleteFile(commandParts[1]);
+                commandBytes = ByteBuffer.wrap(("r" + commandParts[1]).getBytes());
                 break;
             case ("mv"):
-                renameFile(commandParts[1], commandParts[2]);
+                commandBytes = ByteBuffer.wrap(("m" + commandParts[1] + commandParts[2]).getBytes());
                 break;
-            case ("upload"):
-                uploadFile(commandParts[1]);
+            case ("ftp"):
+                commandBytes = ByteBuffer.wrap(("u" + commandParts[1]).getBytes());
                 break;
             case ("exit"):
                 System.exit(0);
@@ -61,6 +79,7 @@ public class Client {
             default:
                 System.out.println("Invalid command");
         }
+        return commandBytes;
     }
 }
 
